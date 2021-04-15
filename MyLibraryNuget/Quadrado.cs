10a4c2800aee3a30d1869c8be65287be65662e22
Diogo Schimmelpfennig <diogo_ti@hotmail.com.br>
2021-04-14 23:38:19 -0400

    Projetos de exemplo

---
+++ MyLibraryNuget/Quadrado.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyLibraryNuget
+{
+    public class Quadrado
+    {
+        public double CalcularArea(double lado)
+        {
+            return lado * lado;
+        }
+    }
+}
